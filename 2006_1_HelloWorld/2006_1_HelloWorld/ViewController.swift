24ba0d53f6ebf059a51cb5a03a3b5e0c6d5fbe4a
Irfan Godinjak <igodinjak@devlogic.eu>
2016-06-19 23:57:01 +0200

    Project created

---
+++ 2006_1_HelloWorld/2006_1_HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2006_1_HelloWorld
+//
+//  Created by Irfan Godinjak on 19/06/16.
+//  Copyright © 2016 WIP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
