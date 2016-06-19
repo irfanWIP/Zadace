84c7e372ca34829d456c6f224ee64167fe1224cc
Irfan Godinjak <igodinjak@devlogic.eu>
2016-06-20 00:12:32 +0200

    Initial Commit

---
+++ HelloMyWorld/HelloMyWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloMyWorld
+//
+//  Created by Irfan Godinjak on 20/06/16.
+//  Copyright © 2016 WIP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
